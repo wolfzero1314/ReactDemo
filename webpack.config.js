6923b879a992952b9727de822d42de85e65fe578
wolfzero1314 <77227370@163.com>
2017-08-02 11:11:10 +0800

    add  css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,16 @@ module.exports = {//注意这里是exports不是export
                 test: /\.(js|jsx)$/,//一个匹配loaders所处理的文件的拓展名的正则表达式，这里用来匹配js和jsx文件（必须）
                 exclude: /node_modules/,//屏蔽不需要处理的文件（文件夹）（可选）
                 loader: 'babel-loader'//loader的名称（必须）
+            },
+            {
+                test: /\.svg$/,
+                exclude: /node_modules/,
+                loader: 'svg-inline-loader'
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                loader: ['style-loader','css-loader']
             }
         ]
     },
@@ -25,11 +35,8 @@ module.exports = {//注意这里是exports不是export
     //webpack-dev-server配置
     devServer: {
         contentBase: './build',//默认webpack-dev-server会为根文件夹提供本地服务器，如果想为另外一个目录下的文件提供本地服务器，应该在这里设置其所在目录（本例设置到"build"目录）
-        //hot:true,
-        //colors: true,//在cmd终端中输出彩色日志,webpack新版本不支持
         historyApiFallback: true,//在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，所有的跳转将指向index.html
         inline: true,//设置为true，当源文件改变时会自动刷新页面
-        port: 3000,//设置默认监听端口，如果省略，默认为"8080"
-        //process: true,//显示合并代码进度，webpack新版本不支持
+        port: 5000,//设置默认监听端口，如果省略，默认为"8080"
     }
 };
